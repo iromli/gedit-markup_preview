644128c5214441ac68b9007f164ce157c6eac7c6
Isman Firmansyah <izman.romli@gmail.com>
2011-07-01 10:31:00 +0700

    cleanup

--- markup_preview/markup_preview.py
+++ markup_preview/markup_preview.py
@@ -31,42 +31,74 @@ from gettext import gettext as _
 
 # Source: http://fgnass.posterous.com/github-markdown-preview
 HTML_TEMPLATE = """<html><head><style type="text/css">
- .github {margin-left:auto;margin-right:auto;padding:0.7em;border:1px solid #E9E9E9;background-color:#f8f8f8;font-size:13.34px;font-family:helvetica,arial,freesans,clean,sans-serif;width:920px;}
- .github h1,h2,h3,h4,h5,h6{border:0;}
- .github h1{font-size:170%%;border-top:4px solid #aaa;padding-top:.5em;margin-top:1.5em;}
- .github h1:first-child{margin-top:0;padding-top:.25em;border-top:none;}
- .github h2{font-size:150%%;margin-top:1.5em;border-top:4px solid #e0e0e0;padding-top:.5em;}
- .github h3{margin-top:1em;}
- .github p{margin:1em 0;line-height:1.5em;}
- .github ul{margin:1em 0 1em 2em;}
- .github ol{margin:1em 0 1em 2em;}
- .github ul li{margin-top:.5em;margin-bottom:.5em;}
- .github ul ul,ul ol,ol ol,ol ul,{margin-top:0;margin-bottom:0;}
- .github blockquote{margin:1em 0;border-left:5px solid #ddd;padding-left:.6em;color:#555;}
- .github dt{font-weight:bold;margin-left:1em;}
- .github dd{margin-left:2em;margin-bottom:1em;}
- .github table{margin:1em 0;}
- .github table th{border-bottom:1px solid #bbb;padding:.2em 1em;}
- .github table td{border-bottom:1px solid #ddd;padding:.2em 1em;}
- .github pre{margin:1em 0;font-size:12px;background-color:#f8f8ff;border:1px solid #dedede;padding:.5em;line-height:1.5em;color:#444;overflow:auto;}
- .github pre code{padding:0;font-size:12px;background-color:#f8f8ff;border:none;}
- .github code{font-size:12px;background-color:#f8f8ff;color:#444;padding:0 .2em;border:1px solid #dedede;}
- .github a{color:#4183c4;text-decoration:none;}
- .github a:hover{text-decoration:underline;}
- .github a code,a:link code,a:visited code{color:#4183c4;}
- .github img{max-width:100%%;}
+    .github {
+        margin-left:auto;
+        margin-right:auto;
+        padding:0.7em;
+        border:1px solid #E9E9E9;
+        background-color:#f8f8f8;font-size:13.34px;
+        font-family:helvetica,arial,freesans,clean,sans-serif;
+        width:920px;
+    }
+    .github h1, h2, h3, h4, h5, h6 {border:0;}
+    .github h1 {
+        font-size:170%%;
+        border-top:4px solid #aaa;
+        padding-top:.5em;
+        margin-top:1.5em;
+    }
+    .github h1:first-child {margin-top:0;padding-top:.25em;border-top:none;}
+    .github h2 {
+        font-size:150%%;
+        margin-top:1.5em;
+        border-top:4px solid #e0e0e0;
+        padding-top:.5em;
+    }
+    .github h3 {margin-top:1em;}
+    .github p {margin:1em 0;line-height:1.5em;}
+    .github ul {margin:1em 0 1em 2em;}
+    .github ol {margin:1em 0 1em 2em;}
+    .github ul li {margin-top:.5em;margin-bottom:.5em;}
+    .github ul ul, ul ol, ol ol, ol ul {margin-top:0;margin-bottom:0;}
+    .github blockquote {
+        margin:1em 0;
+        border-left:5px solid #ddd;
+        padding-left:.6em;
+        color:#555;
+    }
+    .github dt {font-weight:bold;margin-left:1em;}
+    .github dd {margin-left:2em;margin-bottom:1em;}
+    .github table {margin:1em 0;}
+    .github table th {border-bottom:1px solid #bbb;padding:.2em 1em;}
+    .github table td {border-bottom:1px solid #ddd;padding:.2em 1em;}
+    .github pre {
+        margin:1em 0;
+        font-size:12px;
+        background-color:#f8f8ff;
+        border:1px solid #dedede;
+        padding:.5em;
+        line-height:1.5em;
+        color:#444;
+        overflow:auto;
+    }
+    .github pre code {
+        padding:0;
+        font-size:12px;
+        background-color:#f8f8ff;
+        border:none;
+    }
+    .github code {
+        font-size:12px;
+        background-color:#f8f8ff;
+        color:#444;
+        padding:0 .2em;
+        border:1px solid #dedede;
+    }
+    .github a {color:#4183c4;text-decoration:none;}
+    .github a:hover {text-decoration:underline;}
+    .github a code,a:link code,a:visited code{color:#4183c4;}
+    .github img {max-width:100%%;}
 </style></head><body><div class="github">%s</div></body></html>"""
-
-UI = """
-<ui>
-  <menubar name="MenuBar">
-    <menu name="ToolsMenu" action="Tools">
-      <placeholder name="ToolsOps_2">
-        <menuitem name="MP" action="MP"/>
-      </placeholder>
-    </menu>
-  </menubar>
-</ui>"""
 
 MARKUP_CHOICES = {
     'markdown': ['.markdown', '.md', '.mdown', '.mkd', '.mkdn'],
@@ -97,7 +129,6 @@ class MarkupPreview:
     def __init__(self, plugin, window):
         self.window = window
         self.plugin = plugin
-        self.valid_markup = False
         self.activate()
 
     def update_ui(self):
@@ -160,7 +191,6 @@ class MarkupPreview:
         for format, ext in choices:
             if file_ext in ext:
                 markup = format
-                self.valid_markup = True
                 break
 
         text = doc.get_text(start, end)
@@ -173,12 +203,11 @@ class MarkupPreview:
             content = content.get('html_body')
         elif markup == 'textile':
             content = textile.textile(text)
-
-        if not markup:
+        else:
             return
 
         html = HTML_TEMPLATE % (content,)
-        window_data['web_view'].load_string(html,'text/html', 'iso-8859-15',
+        window_data['web_view'].load_string(html, 'text/html', 'iso-8859-15',
             'about:blank')
 
         bottom = self.window.get_bottom_panel()
